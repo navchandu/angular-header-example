9894cd168c875646549fee2b5c39442c38618e80
navchandu <chandrika.navsupe@gmail.com>
2024-02-11 21:44:02 +0530

    first commit

---
+++ src/app/header/header.component.ts
@@ -0,0 +1,20 @@
+import { Component ,OnInit} from '@angular/core';
+
+@Component({
+  selector: 'app-header',
+  standalone: true,
+  imports: [],
+  templateUrl: './header.component.html',
+  styleUrl: './header.component.css'
+})
+export class HeaderComponent {
+
+  constructor() {
+    // Constructor logic goes here
+  }
+
+  ngOnInit(): void {
+    // Initialization logic goes here
+  }
+}
+
